3561667b444f6a4c5f5d1ff1938621b5ea690069
Achim Passen <achim.passen@gmail.com>
2014-08-25 07:53:02 +0200

    more refactoring

--- src/opt/core.clj
+++ src/opt/core.clj
@@ -2,62 +2,10 @@
   (:require [clojure.math.numeric-tower :as num]
             [opt.util :as util]
             [opt.cbc :as cbc]
-            [opt.problems :as problems]
-            [opt.types :as types])
+            [opt.problems :as problems])
   (:use plumbing.core))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(def ^:private make-id
-  (let [max (num/expt 10 15)
-        ctr (atom 0)
-        nxt (fn [i] (mod (inc i) max))]
-    (fn [] (swap! ctr nxt))))
-
-(defn variable [& {:keys [type tag] :or {type :gen} :as args}]
-  (types/->Variable (make-id) type (:>= args) (:<= args) tag nil))
-
-(defn linear-term
-  ([vars->coeffs] (types/->LinearTerm (map-keys :id vars->coeffs)))
-  ([vars coeffs] (linear-term (zipmap vars coeffs))))
-
-(defn negate [linear-term]
-  (update-in linear-term [:varids->coeffs] #(map-vals - %)))
-
-(defn affine-constr
-  ([ineq lhs rhs]
-     (if (= ineq :<=)
-       (types/->AffineConstr lhs rhs)
-       (types/->AffineConstr (negate lhs) (- rhs)))))
-
-(defn problem [flavor objective constraints vars]
-  (types/->Problem flavor
-                   objective
-                   (flatten constraints)
-                   (util/index-by-unique :id vars)
-                   (->> vars
-                        (filter :tag)
-                        (util/index-by-unique :tag)
-                        (map-vals :id))
-                   :unattempted
-                   nil))
-
-(defn update-problem [problem solver]
-  (let [result       (solver problem)
-        status       (:status result)
-        val          (:val result)
-        varids->vals (:varids->vals result)
-        problem      (-> problem
-                         (assoc :status status)
-                         (assoc :val val))
-        step         (fn [acc [vid val]]
-                  (update-in acc [:varids->vars vid] assoc :val val))]
-    (reduce step problem varids->vals)))
-
-(defn tag->var [problem tag]
-  (let [varids->vars (:varids->vars problem)
-        tags->varids (:tags->varids problem)]
-    (-> tag tags->varids varids->vars)))
 
 #_(
 
